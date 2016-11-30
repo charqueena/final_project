1c1131e7299f9be0babd328ead83652770d1ed4f
Charlene Nuval <charputer@Charlenes-MacBook-Pro.local>
2016-11-29 20:02:23 -0800

    first commit

---
+++ Testing/testing.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+function submitAnswers(){
+var total = 3;
+var score = 0;
+
+//Get user input
+var q1 = document.forms["quizForm"]["q1"].value;
+var q2 = document.forms["quizForm"]["q2"].value;
+var q3 = document.forms["quizForm"]["q3"].value;
+
+alert(q1);
+
+}
+
+
+// console.log("hello");
+
+});
