c1e248a955fc0a17b470fb4f54064d4d92d73b9d
Charlene Nuval <charputer@Charlenes-MacBook-Pro.local>
2016-12-02 19:54:12 -0800

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-// On click, select answer, next question will slide down/show. Only did this up to question 6, will update all later.
+  $("body").fadeIn(2000);
+  // On click, select answer, next question will slide down/show. Only did this up to question 6, will update all later.
 
   $(".questionOne-container").click(function(){
     $(".questionTwo-container").slideDown(500);
@@ -26,28 +27,32 @@ $(document).ready(function() {
 
   });
 
+  $(".questionSix-container").click(function(){
+    $(".questionSeven-container").slideDown(500).show();
+
+  });
+  $(".questionSeven-container").click(function(){
+    $(".questionEight-container").slideDown(500).show();
+
+  });
+  $(".questionEight-container").click(function(){
+    $(".questionNine-container").slideDown(500).show();
+
+  });
+  $(".questionNine-container").click(function(){
+    $(".questionTen-container").slideDown(500).show();
+
+  });
+
+
+
+
+
+
+
+
+
+
   // Question 10 div to have logic. For each input checked, show associated results_div
 
-  $('#submit_last').on("click" function() {
-    if ($("#q10a:checked").val() == "a" {
-      $(".results_strong-mad").show();
-    });
-
-    $('#submit_last').on("click" function() {
-      if ($("#q10b:checked").val() == "b" {
-        $(".results_strong-bad").show();
-      });
-      $('#submit_last').on("click" function() {
-        if ($("#q10c:checked").val() == "c" {
-          $(".results_strong-sad").show();
-        });
-
-        $('#submit_last').on("click" function() {
-          if ($("#q10d:checked").val() == "d" {
-            $(".results_the-cheat").show();
-          });
-
-
-        });
-
-      });
+});
--- quizlogic.js
+++ quizlogic.js
@@ -1,43 +1,44 @@
 $(document).ready(function() {
 
-
+  $("body").fadeIn(2000);
   //Set up our variables to use later
-var result;
+  var result;
 
-//Get a random value for the computer choice
-var resultVal = Math.random();
-
-
-$("#submit").on("click", function() {
-  alert(resultVAl);
-
-  //based on question 10
-  //if select a, b, c, d, show associated character
-
-//Turn the random value into a choice for the computer
+  //Get a random value for the computer choice
+  var resultVal = Math.random();
 
 
-  // function submitAnswers(){
-  //
-  // var total = 10;
-  // var score = 0;
-  //
-  // // Get user input
-  //
-  // var qu1 = $('input').val(); // assumes one input element
-  // // var q1= document.forms.label["questionOne-responses"]["optradio"].value;
-  // // var q2= document.div["questionTwo-responses"]["q2"].value;
-  // // var q3= document.div["questionThree-responses"]["q3"].value;
-  // // var q4= document.div["questionFour-responses"]["q4"].value;
-  // // var q5= document.div["questionFive-responses"]["q5"].value;
-  // // var q6= document.div["questionSix-responses"]["q6"].value;
-  // // var q7= document.div["questionSeven-responses"]["q7"].value;
-  // // var q8= document.div["questionEight-responses"]["q8"].value;
-  // // var q9= document.div["questionNine-responses"]["q9"].value;
-  // // var q10= document.div["questionTen-responses"]["q10"].value;
-  // alert(qu1);
+  $("#submit").on("click", function() {
+    alert(resultVAl);
 
-// }
+    //based on question 10
+    //if select a, b, c, d, show associated character
 
-// doc ready
-});
+    //Turn the random value into a choice for the computer
+
+
+    // function submitAnswers(){
+    //
+    // var total = 10;
+    // var score = 0;
+    //
+    // // Get user input
+    //
+    // var qu1 = $('input').val(); // assumes one input element
+    // // var q1= document.forms.label["questionOne-responses"]["optradio"].value;
+    // // var q2= document.div["questionTwo-responses"]["q2"].value;
+    // // var q3= document.div["questionThree-responses"]["q3"].value;
+    // // var q4= document.div["questionFour-responses"]["q4"].value;
+    // // var q5= document.div["questionFive-responses"]["q5"].value;
+    // // var q6= document.div["questionSix-responses"]["q6"].value;
+    // // var q7= document.div["questionSeven-responses"]["q7"].value;
+    // // var q8= document.div["questionEight-responses"]["q8"].value;
+    // // var q9= document.div["questionNine-responses"]["q9"].value;
+    // // var q10= document.div["questionTen-responses"]["q10"].value;
+    // alert(qu1);
+
+    // }
+
+    // doc ready
+  });
+    });
